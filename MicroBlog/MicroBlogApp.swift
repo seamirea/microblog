e5b11d0dc79a2d59e9ed4edf77a664103e6f5d9f
seamirea <amira.akybay@gmail.com>
2022-12-31 20:48:45 +0600

    Initial Commit

---
+++ MicroBlog/MicroBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroBlogApp.swift
+//  MicroBlog
+//
+//  Created by Акыбай Амира  on 31.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct MicroBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
